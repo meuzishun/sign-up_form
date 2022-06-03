fdad6ddf56c4da6a1c3fb34b9d81bb5dbc63467d
Andrew Smith <meuzishun@gmail.com>
2022-06-03 15:43:32 -0400

    Create more elaborate form validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,34 @@
 const validateForm = function (e) {
-  e.preventDefault();
   const form = e.target;
-  console.log(form);
+  const password = form.querySelector('#password');
+  const confirmPassword = form.querySelector('#confirm-password');
 
   if (form.checkValidity()) {
+    if (password.value !== confirmPassword.value) {
+      const helpMsg = confirmPassword.parentElement.querySelector('.help');
+      helpMsg.textContent = 'Passwords must match';
+      confirmPassword.parentElement.classList.add('invalid');
+      e.preventDefault();
+      return;
+    }
     console.log('form is good');
   } else {
+    e.preventDefault();
     console.log('form is bad');
     Array.from(form.elements).forEach((elem) => {
+      if (elem.type === 'submit') {
+        return;
+      }
+      const inputContainer = elem.parentElement;
+      const helpMsg = inputContainer.querySelector('.help');
       if (elem.checkValidity()) {
-        elem.parentElement.classList.remove('invalid');
+        inputContainer.classList.remove('invalid');
+        helpMsg.textContent = '';
         return;
       } else {
-        console.log(elem);
-        elem.parentElement.classList.add('invalid');
+        inputContainer.classList.add('invalid');
+        helpMsg.textContent = elem.validationMessage;
+        return;
       }
     });
   }
